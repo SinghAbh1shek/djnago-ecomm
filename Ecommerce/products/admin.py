c635033cd904d45604c7516d8acf1051749cc34c
Abhishek Singh <abhisheksingh.muj@gmail.com>
2025-10-16 13:46:01 +0530

    register products to admin

--- Ecommerce/products/admin.py
+++ Ecommerce/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import *
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(BrandName)
+admin.site.register(Product)
+admin.site.register(VariantOptions)
+admin.site.register(ProductImages)
+admin.site.register(VendorProducts)

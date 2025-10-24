9201d2fec0972ad96dc0a217a4d1ebd650f563bb
Abhishek Singh <abhisheksingh.muj@gmail.com>
2025-10-24 15:18:26 +0530

    some changes

--- Ecommerce/accounts/models.py
+++ Ecommerce/accounts/models.py
@@ -5,10 +5,17 @@ from django.contrib.auth.models import User
 class Customer(User):
     profile_image = models.ImageField(upload_to='customer/', null=True, blank=True)
 
+    class Meta:
+        db_table = "Customer"
+    
+
 class Shopkeeper(User):
     gst_number = models.CharField(max_length=15)
     adhar_number = models.CharField(max_length=14)
     profile_image = models.ImageField(upload_to='customer/', null=True, blank=True)
     bmp_id = models.CharField(max_length=100, unique=True)
     vendor_name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "Shopkeeper"
     
